3211c3c7de75cbc1f4b09ab3921bc8e6aa71eaec
cola2261 <cola2261@proton.me>
2025-10-29 00:38:10 +0500

    dm2q: Move some OFOX settings to common tree https://github.com/cola2261/android_device_samsung_sm8550-common-recovery/tree/fox_12.1
    Signed-off-by: cola2261 <cola2261@proton.me>

--- fox_dm2q.mk
+++ fox_dm2q.mk
@@ -24,17 +24,12 @@
 # OrangeFox settings
 OF_HIDE_NOTCH := 1
 OF_USE_GREEN_LED := 0
-OF_FLASHLIGHT_ENABLE := 1
 OF_ALLOW_DISABLE_NAVBAR := 0
-OF_SKIP_FBE_DECRYPTION := 1
 
 # Maximum permissible splash image size (in kilobytes); do *NOT* increase
 OF_SPLASH_MAX_SIZE := 130
 
 OF_ADVANCED_SECURITY := 1
-
-# Maintainer
-OF_MAINTAINER := cola2261
 
 # OrangeFox GUI settings
 # dm2q
@@ -44,9 +39,3 @@ OF_STATUS_INDENT_LEFT := 68
 OF_STATUS_INDENT_RIGHT := 68
 OF_CLOCK_POS := 1
 OF_OPTIONS_LIST_NUM := 6
-
-# lz4 compression
-OF_USE_LZ4_COMPRESSION := 1
-
-# LPTools
-OF_ENABLE_LPTOOLS := 1
