8fe53191dd6223af6d542ebdc078d1a40b2ade2e
cola2261 <cola2261@proton.me>
2025-11-04 19:22:09 +0500

    dm2q: Adjust status bar height
    Signed-off-by: cola2261 <cola2261@proton.me>

--- fox_dm2q.mk
+++ fox_dm2q.mk
@@ -25,15 +25,14 @@
 OF_HIDE_NOTCH := 1
 OF_USE_GREEN_LED := 0
 OF_ALLOW_DISABLE_NAVBAR := 0
+OF_ADVANCED_SECURITY := 1
 
 # Maximum permissible splash image size (in kilobytes); do *NOT* increase
 OF_SPLASH_MAX_SIZE := 130
 
-OF_ADVANCED_SECURITY := 1
-
 # OrangeFox GUI settings
 OF_SCREEN_H := 2340
-OF_STATUS_H := 94 #74
+OF_STATUS_H := 92 # 74 18
 OF_STATUS_INDENT_LEFT := 68
 OF_STATUS_INDENT_RIGHT := 68
 OF_CLOCK_POS := 1
